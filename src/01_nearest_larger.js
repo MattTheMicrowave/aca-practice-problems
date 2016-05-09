138ee7729d2b90ed6d7e4a1126229b83732a8857
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-08 22:51:05 -0500

    nil

--- src/01_nearest_larger.js
+++ src/01_nearest_larger.js
@@ -23,8 +23,8 @@ function nearestLarger(arr, i) {
 // this returns null if we have reached each end of the array without finding
 // a greater value
             else if ((jL < 0) && (jR >= arr.length)) {
-              
-              return null;
+
+              return;
             }
           }
 }
