5d94e8efea6022cd43beca313f2d08cd0aaac0d3
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-08 23:07:35 -0500

    nil

--- src/01_nearest_larger.js
+++ src/01_nearest_larger.js
@@ -5,7 +5,7 @@ function nearestLarger(arr, i) {
 
 // var j is undefined, this will be defined later
 
-
+          
 // vars jL and jR increment out from the given index
           var jL = i - x;
           var jR = i + x;
@@ -22,11 +22,10 @@ function nearestLarger(arr, i) {
             }
 // this returns null if we have reached each end of the array without finding
 // a greater value
-            else {
+            else if ((jL < 0) && (jR >= arr.length)) {
 
               return null;
             }
-
           }
 }
 
