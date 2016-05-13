d7e5e84d5510278e80fc5da744b7dd64f57daeab
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-13 13:00:18 -0500

    no repeats so far

--- src/02_no_repeats.js
+++ src/02_no_repeats.js
@@ -1,5 +1,25 @@
 function noRepeats(yearStart, yearEnd) {
+var outputArray = [];
+    for (x = yearStart; x <= yearEnd; x++) {
+      function makingTestingArray(x) {
+      var stringified = x.toString();
+      var numArray = stringified.split("");
+      var sortedNumArray = numArray.sort();
 
+
+        for (i = 0; i < sortedNumArray.length - 1; i++) {
+        if (sortedNumArray[i] === sortednumArray[i + 1]) {
+
+          return false;
+        } else {
+
+          outputArray = outputArray.push(x);
+        }
+}
+
+    }
+  }
+    return outputArray;
 }
 
 module.exports = noRepeats;
