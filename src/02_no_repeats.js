367318833144b57484f3a844639609c54cff58de
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-13 13:19:53 -0500

    ver 2.3

--- src/02_no_repeats.js
+++ src/02_no_repeats.js
@@ -1,21 +1,25 @@
 function noRepeats(yearStart, yearEnd) {
 var outputArray = [];
-    for (x = yearStart; x <= yearEnd; x++) {
-      var year = x;
-      function makingTestingArray(x) {
-      var stringified = x.toString();
-      var numArray = stringified.split("");
-      var sortedNumArray = numArray.sort();
+var princip = yearStart;
+var fin = yearEnding;
+    // for (x = yearStart; x <= yearEnd; x++)
+    //   var year = x;
+      function makingTestingArray(princip, fin) {
+        for (x = princip; x <= fin; x++) {
+            var year = x;
+            var stringified = x.toString();
+            var numArray = stringified.split("");
+            var sortedNumArray = numArray.sort();
 
 
         for (i = 0; i < sortedNumArray.length - 1; i++) {
-        if (sortedNumArray[i] = sortednumArray[i + 1]) {
+            if (sortedNumArray[i] = sortednumArray[i + 1]) {
 
-          return false;
-        } else {
+              return false;
+            } else {
 
-          outputArray = outputArray.push(year);
-        }
+              outputArray = outputArray.push(year);
+            }
 }
 
     }
