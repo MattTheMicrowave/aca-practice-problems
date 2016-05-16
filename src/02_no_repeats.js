25f5949641f08cc2c1980024cf843bfe39d28cf3
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-16 18:26:00 -0500

    ver ?

--- src/02_no_repeats.js
+++ src/02_no_repeats.js
@@ -2,7 +2,7 @@ function noRepeats(yearStart, yearEnd) {
     var outputArray = [];
     var princip = yearStart;
     var fin = yearEnd;
-    
+
       function makingTestingArray(princip, fin) {
         for (x = princip; x <= fin; x++) {
             var year = x;
@@ -12,7 +12,7 @@ function noRepeats(yearStart, yearEnd) {
             outputArray = outputArray.push(year);
 
         for (i = 0; i < sortedNumArray.length - 1; i++) {
-            if (sortedNumArray[i] = sortednumArray[i + 1]) {
+            if (sortedNumArray[i] === sortednumArray[i + 1]) {
 
               outputArray.pop();
             }
