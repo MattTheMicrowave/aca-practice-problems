7dc29f489f1cfb888b3b35be1960f5a8d634d3c0
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-20 14:30:19 -0500

    ver 4.0

--- src/03_letter_count.js
+++ src/03_letter_count.js
@@ -2,22 +2,36 @@ function letterCount(str) {
 
     var strArray = str.split("");
     var letterCountObject = {};
-    for (i = 0;i = strArray.length - 1; i++) {
+
+    for (i = 0; i < strArray.length; i++) {
         var letra = strArray[i];
-        if (letterCountObject[letra]) {
 
-            letterCountObject[letra] += 1;
-
-        } else {
+        if (frequencyCount(letra)) {
 
             letterCountObject[letra] = 1;
+
         }
 
-
-
+            letterCountObject[letra] += 1;
 
     }
     return letterCountObject;
 }
+
+function frequencyCount(x) {
+    var frequencyCountObject = {};
+
+      if (frequencyCountObject[x]) {
+          return false;
+
+      } else {
+
+          frequencyCountObject[x] = true;
+
+      }
+
+      return true;
+  }
+
 
 module.exports = letterCount;
