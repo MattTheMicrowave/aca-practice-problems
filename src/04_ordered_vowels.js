746aaa06eac2c6707735d64c010c108885f10cb8
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-28 19:29:06 -0500

    very rough draft

--- src/04_ordered_vowels.js
+++ src/04_ordered_vowels.js
@@ -1,5 +1,47 @@
 function orderedVowelWords(str) {
+    while (str.indexOf(" ")) {
+      
+    }
+    if (orderedVowelWord(str)) {
 
+      return str;
+
+    } else {
+
+      return false;
+
+    }
 }
+
+function orderedVowelWord(word) {
+    var letterArray = word.split("");
+
+    for (i = 0; i = letterArray.length - 1; i++) {
+
+        if (letterArray[i] === 'a') {
+            return true;
+        }
+        else if ( (letterArray[i] === 'e') && ( (word.indexOf('a') ) < (word.indexOf('e') ) ) ) {
+            return true;
+        }
+        else if ( (letterArray[i] === 'i') && ( (word.indexOf('e') ) < (word.indexOf('i') ) ) ) {
+            return true;
+        }
+        else if ( (letterArray[i] === 'o') && ( (word.indexOf('i') ) < (word.indexOf('o') ) ) ) {
+            return true;
+        }
+        else if ( (letterArray[i] === 'u') && ( (word.indexOf('o') ) < (word.indexOf('u') ) ) ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    }
+
+
+
+
 
 module.exports = orderedVowelWords;
