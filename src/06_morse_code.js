16b7c15e1abd6481ac4cbede913ac90f600c8aaa
MattTheMicrowave <mwlluis@hotmail.com>
2016-06-11 18:56:43 -0500

    done

--- src/06_morse_code.js
+++ src/06_morse_code.js
@@ -1,34 +1,29 @@
 var codigo = {
-  a:".-",b:"-...",c:"-.-.",d:"-..",e:".",f:"..-.",g:"--.",h:"....",i:"..",j:".---",k:"-.-",l:".-..",m:"--",n:"-.",o:"---",p:".--."
-  q:"--.-",r:".-.",s:"...",t:"-",u:"..-",v:"...-",w:".--",x:"-..-",y:"-.--",z:"--.."
-
-}
+  "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g":"--.",
+  "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.",
+  "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-",
+  "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--.." };
 
 
 function morseEncode(n) {
-  var outPutString = "";
+
   var arrayOfWords = n.split(" ");
-  for (j = 0; j = array.length - 1; i++) {
-      var addOn = translateWord(i).concat("  ");
-      var outPutString = outPutString.concat(addOn);
-  }
+  var arrayOfTranslatedWords = arrayOfWords.map(translateWord);
+  var outPutString = arrayOfTranslatedWords.join("  ");
   return outPutString;
 }
 
 
 function translateWord(word) {
-    var letters = word.split('');
-    var translatedLetters = [];
-          for (i = 0; i = letters.length - 1; i++) {
-              if (codigo[letters[i]]) {
+    var arrayOfLetters = word.split("");
+    var arrayOfTranslatedLetters = arrayOfLetters.map(translator);
+    var outPutWordString = arrayOfTranslatedLetters.join(" ");
+    return outPutWordString;
 
-                  var alpha = codigo[letters[i]];
-                  var translatedLetters = translatedLetters.push(alpha);
-              }
-          var translatedWord = translatedLtters.join(" ");
-          return translatedWord;
-          }
 }
 
+function translator(x) {
+  return codigo[x];
 
+}
 module.exports = morseEncode;
