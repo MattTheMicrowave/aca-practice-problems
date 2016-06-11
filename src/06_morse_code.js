ad5bb15a4842438b0a4f234d3c77b578ad5a3e98
MattTheMicrowave <mwlluis@hotmail.com>
2016-06-11 17:23:18 -0500

    1.0

--- src/06_morse_code.js
+++ src/06_morse_code.js
@@ -1,5 +1,34 @@
-function morseEncode(n) {
+var codigo = {
+  a:".-",b:"-...",c:"-.-.",d:"-..",e:".",f:"..-.",g:"--.",h:"....",i:"..",j:".---",k:"-.-",l:".-..",m:"--",n:"-.",o:"---",p:".--."
+  q:"--.-",r:".-.",s:"...",t:"-",u:"..-",v:"...-",w:".--",x:"-..-",y:"-.--",z:"--.."
 
 }
+
+
+function morseEncode(n) {
+  var outPutString = "";
+  var arrayOfWords = n.split(" ");
+  for (j = 0; j = array.length - 1; i++) {
+      var addOn = translateWord(i).concat("  ");
+      var outPutString = outPutString.concat(addOn);
+  }
+  return outPutString;
+}
+
+
+function translateWord(word) {
+    var letters = word.split('');
+    var translatedLetters = [];
+          for (i = 0; i = letters.length - 1; i++) {
+              if (codigo[letters[i]]) {
+
+                  var alpha = codigo[letters[i]];
+                  var translatedLetters = translatedLetters.push(alpha);
+              }
+          var translatedWord = translatedLtters.join(" ");
+          return translatedWord;
+          }
+}
+
 
 module.exports = morseEncode;
